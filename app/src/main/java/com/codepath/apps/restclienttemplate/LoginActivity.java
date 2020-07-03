6796490c5828386820423197cbfa0f3b51664daa
Alex Madoff <a1madoff@gmail.com>
2020-07-02 23:46:55 -0700

    Added gif with login

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
